8ca6439ae6728008a1ea8a4ebcd7471f3c7abf29
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-07-03 01:35:49 +0500

    - Auth views implemented. - Added views for login, logout, and registration. - Added a custom user model with email as the primary identifier.

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('auth/obtain-auth-token/', obtain_auth_token),
+    path('auth/delete-auth-token/', views.logout),
+    path('auth/register/',views.UserRegistrationView.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import UserSerializer
+from rest_framework.views import APIView
 # Create your views here.
+
+@api_view(['POST'])
+def logout(request):
+    try:
+        token = request.data['TOKEN']
+        token = Token.objects.get(key=token)
+        token.delete()
+        return Response({'message':'Logout Successful'}, status=status.HTTP_202_ACCEPTED)
+    except:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+class UserRegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
