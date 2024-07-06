7ed6629305cc36494e4b6a79fa71c6fcaba92b8b
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-07-06 13:08:17 +0500

    Incomplete frontend

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,6 +2,8 @@ import Navbar from "./Navbar"
 import Footer from "./Footer"
 import NotLoggedIn from "./NotLoggedIn";
 import { useProjectContext } from "../Context/ProjectContext";
+import { IoIosLaptop } from "react-icons/io";
+
 export default function Home() {
     const {login} = useProjectContext();
     if (!login) {
@@ -12,9 +14,33 @@ export default function Home() {
         <div className="container">
             <Navbar active={'Home'} />
             <div className="home-container">
+                <header>
+                    <h3 className="home-heading">
+                        Your All-in-One Project Management Tool for Seamless Collaboration and Success
+                    </h3>
+                </header>
                 <main>
-                    <h1 className="pg-heading">Welcome to Project Management Tool</h1>
-                    <p className="hd-desc">Manage your projects and users efficiently</p>
+                    <div className="card-container">
+                        <div className="card">
+                            <div className="left-side">
+                                <div>
+                                    <IoIosLaptop className="card-icon" />
+                                    <h5 className="card-heading">Projects</h5>
+                                </div>
+                            </div>
+                            <div className="right-side">
+                                
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="left-side">
+                                <IoIosLaptop className="card-icon" />
+                            </div>
+                            <div className="right-side">
+                                
+                            </div>
+                        </div>
+                    </div>
                 </main>
             </div>
             <Footer />
