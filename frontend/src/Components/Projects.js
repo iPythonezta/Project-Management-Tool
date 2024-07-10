3163a4ed7a60e251132a38362ecced0d62f9d5a1
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-07-10 14:40:59 +0500

    Basic design of projects page

--- frontend/src/Components/Projects.js
+++ frontend/src/Components/Projects.js
@@ -9,6 +9,33 @@ export default function Projects() {
             <h2 className="pg-heading">
                 Projects
             </h2>
+            <div className="button-justifier">
+                <button className="gray-button">Add Project</button>
+            </div>
+            <table className="proj-table" border={1}>
+                <thead>
+                    <tr>
+                        <th colSpan={1}>ID</th>
+                        <th colSpan={2}>Project Title</th>
+                        <th colSpan={3}>Project Description</th>
+                        <th colSpan={1.5}>Start Date</th>
+                        <th colSpan={1.5}>End Date</th>
+                        <th colSpan={1.5}>Created At</th>
+                        <th colSpan={1.5}>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colSpan={1}>1</td>
+                        <td colSpan={2}>Testing Project Styling</td>
+                        <td colSpan={3}>Styling the projects page....</td>
+                        <td colSpan={1.5}>07/07/2024</td>
+                        <td colSpan={1.5}>09/07/2024</td>
+                        <td colSpan={1.5}>06/07/2023</td>
+                        <td colSpan={1.5}>In Progress</td>
+                    </tr>
+                </tbody>
+            </table>
             <Footer />
         </div>
     )
